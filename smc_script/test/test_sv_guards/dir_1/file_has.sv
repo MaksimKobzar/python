2bae1e9dd78ff208d54e455bc1e1f404ae197633
m_kobzar <yogurtstarlite@gmail.com>
2020-11-26 21:01:22 +0300

    [smc_script] add tests for smc_sv_guards.py

---
+++ smc_script/test/test_sv_guards/dir_1/file_has.sv
@@ -0,0 +1,78 @@
+// ----------------------------------------------------------------------------
+// AUTHOR'S EMAIL : kobzar
+// ----------------------------------------------------------------------------
+// DESCRIPTION    :
+// ----------------------------------------------------------------------------
+
+/*
+
+*/
+
+// cqwq
+// ----------
+
+/*
+
+swwqs
+
+*/
+
+
+`ifndef INC_SR_ETH_DR_SEQ
+// `define INC_SR_ETH_DR_SEQ
+
+`define INC_SR_ETH_DR_SEQ
+
+/*
+dwqwq
+
+*/
+
+/*
+
+*/
+
+class sr_eth_dm_seq extends sr_eth_base_seq;
+
+  sr_eth_dm dm;
+  sr_eth_fm fm;
+
+  `uvm_object_utils(sr_eth_dm_seq)
+
+  function new(string name ="sr_eth_dm_seq");
+    super.new(name);
+  endfunction
+
+  task pre_body();
+    uvm_mem mem;
+    super.pre_body();
+    if(dm == null) `uvm_fatal(get_full_name(), $sformatf("dm should be set in %0s", get_full_name()))
+    if(fm == null) `uvm_fatal(get_full_name(), $sformatf("fm should be set in %0s", get_full_name()))
+  endtask
+
+  task body();
+    `uvm_info(get_full_name(), "is started", UVM_MEDIUM)
+
+    forever begin
+      fm.get_next_item(frame);
+      dm.add_dm_item(frame);
+      fm.item_done();
+      fm.put_response(frame);
+    end
+
+    `uvm_info(get_full_name(), "is ended", UVM_MEDIUM)
+  endtask
+
+endclass
+
+
+`endif
+
+/*
+faweqf
+
+qwd
+wq
+wq
+
+*/
